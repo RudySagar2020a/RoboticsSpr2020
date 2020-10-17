6bf537206d973bd1c38521200a0346064325f4a9
RudySagar2020a <61991445+RudySagar2020a@users.noreply.github.com>
2020-10-17 17:04:29 +1100

    Add files via upload

--- ENVIRONMENT.m
+++ ENVIRONMENT.m
@@ -5,15 +5,41 @@
 %% Setup
 
 close all
+set(0,'DefaultFigureWindowStyle','docked')
 clc
 clf
 clear
 
-set(0,'DefaultFigureWindowStyle','docked')
-
 %% Parameters
 
 % workspace = [-x x -y y -z z]
-workspace = [-10 10 -5 5 0 5];
+workspace = [-500 500 -100 100 0 100];
+axis equal;
 
 %% Environment
+
+% Table setup
+[f,v,data] = plyread('table.ply','tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue]/255;
+
+% ---- this part of code does not work ----------------------
+%Table start position at(0,0,100)
+tablePose = eye(4);
+forwardTR = makehgtform('translate',[0,0,100]);
+% -----------------------------------------------------------
+hold on;
+
+% PLOT TRISURF
+tableMesh_h = trisurf(f,v(:,1),v(:,2), v(:,3) ...
+    ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
+
+%%
+
+% Bottles setup
+[f,v,data] = plyread('beer_bottle.ply','tri');
+vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue]/255;
+hold on;
+
+% PLOT TRISURF
+beer_bottleMesh_h = trisurf(f,v(:,1),v(:,2), v(:,3) ...   
+    ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
